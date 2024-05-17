18f8fdcaa6eb56396dec502ee4d2563a0d0b73ac
theEast17 <nagarpoorv2001@gmail.com>
2024-05-17 22:44:41 +0530

    Hero complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,55 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { heroVideo,smallHeroVideo } from "../utils"
+import { useState } from "react"
+import { useEffect } from "react"
+
 
 const Hero = () => {
+
+  const [videoSrc,setVideoSrc]=useState(window.innerWidth<760?smallHeroVideo:heroVideo)
+  useGSAP(()=>{
+    gsap.to('#title',{
+      opacity:1,
+      delay:2
+    })
+    gsap.to('#cta',{
+      opacity:1,
+      y:-50,
+      delay:2
+    })
+  },[])
+
+ const videoSetFunction=()=>{
+    if(window.innerWidth<760){
+      setVideoSrc(smallHeroVideo)
+    }else{
+      setVideoSrc(heroVideo)
+    }
+  }
+ 
+
+  useEffect(()=>{
+    window.addEventListener('resize',videoSetFunction)
+    return ()=> window.removeEventListener('resize',videoSetFunction)
+  },[])
+
   return (
-    <div>Hero</div>
+    <section className="w-full bg-black nav-height relative">
+      <div className="w-full h-5/6 flex-center flex-col">
+        <p id="title" className="hero-title">iPhone15 Pro</p>
+        <div className="w-9/12 md:w-10/12">
+          <video className="pointer-events-none" autoPlay muted key={videoSrc} playsInline={true}>
+            <source src={videoSrc} type="video/mp4"/>
+          </video>
+        </div>
+      </div>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/ month or $999</p>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,35 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
 
 const Highlights = () => {
+  useGSAP(()=>{
+    gsap.to('#title',{
+      y:0,
+      opacity:1,
+    })
+    gsap.to('.link',{
+      y:0,
+      opacity:1,
+      duration:1,
+      stagger:0.25
+    })
+  },[])
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-full h-full overflow-hidden bg-zinc common-padding">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between">
+          <h1 id="title" className="section-heading">Get the hightligths</h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">Watch the film <img src={watchImg} alt="watch" className="ml-2" /></p>
+            <p className="link">Watch the event <img src={rightImg} alt="watch" className="ml-2" /></p>
+          </div>
+        </div>
+
+        <VideoCarousel/>
+      </div>
+    </section>
   )
 }
 
